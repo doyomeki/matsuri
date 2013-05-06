fae095ff7204321ff305ea1719ab1cea630ac177
tenten0213 <takehito.0213@gmail.com>
2013-05-07 01:20:36 +0900

    Rolifyに依存しないように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :role
-
-  rolify
-  attr_accessible :role_ids, :as => :admin
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :role, :as => :admin
 
   def admin?
     role == "admin"
