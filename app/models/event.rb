22136470bdd7198c61a32f415f8458f82f33c75a
mayu <white_hot_chocolate@i.softbank.jp>
2013-05-09 18:41:15 +0900

    直近のイベント情報を取得

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ActiveRecord::Base
   attr_accessible :capacity, :description, :end_datetime, :place, :start_datetime, :title
+
+  def self.recent
+    recent_time = self.minimum("start_datetime", :conditions => ["start_datetime > ?", Time.now])
+    self.find_by_start_datetime(recent_time)
+  end
 end
