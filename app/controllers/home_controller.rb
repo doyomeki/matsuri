89f2f183a8a775b2cc455e148aa33724e8fff154
mayu <white_hot_chocolate@i.softbank.jp>
2013-05-09 18:54:34 +0900

    home接続で直近のイベント情報を表示するように変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
+
+    @event = Event.recent
+    if @event
+      redirect_to :controller => "events", :action => "show", :id => @event.id
+    end
   end
 end
