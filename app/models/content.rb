37f9bcebec0b67344faefb5e13b5e131d66495a3
mayu <white_hot_chocolate@i.softbank.jp>
2013-05-10 12:53:54 +0900

    タイムスケジュールに対するセッションの情報を表示

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,4 +2,12 @@ class Content < ActiveRecord::Base
   attr_accessible :capacity, :description, :room_name, :schedule_id, :speaker_id, :title
 
   belongs_to :schedule, foreign_key: "schedule_id"
+
+  def room_name
+    Room.find(self.id).name
+  end
+
+  def room_capacity
+    Room.find(self.id).capacity
+  end
 end
