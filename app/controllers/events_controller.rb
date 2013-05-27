d8267ab47637451b59f33c799518c826410688a5
kaekasui <kaekasui_github@icloud.com>
2013-05-27 10:17:37 +0900

    参加予定のセッション登録のメッセージ追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -37,45 +37,53 @@ class EventsController < ApplicationController
   end
 
   def update
-    # TODO: transactionを追加する
+    ActiveRecord::Base.transaction do
 
-    # イベントの参加者を登録
-    entry_event = user_entry?(params[:id], current_user.id)
-    if entry_event.blank?
-      event_participation = EventParticipation.new(event_id: params[:id], user_id: current_user.id)
-      event_participation.save!
-      event_participation.reload!
-    else
-      entry_event.first.cancelled = nil
-      entry_event.first.save!
-    end
+      # イベントの参加者を登録
+      entry_event = user_entry?(params[:id], current_user.id)
+      if entry_event.blank?
+        event_participation = EventParticipation.new(event_id: params[:id], user_id: current_user.id)
+        event_participation.save!
+        event_participation.reload!
+      else
+        entry_event.first.cancelled = nil
+        entry_event.first.save!
+      end
 
-    # 参加コンテンツを登録
-    event_participation_id = user_entry?(params[:id], current_user.id).first.id
-    schedules = Schedule.where(event_id: params[:id])
-    schedules.each do |schedule|
-      # ラジオボタンで選択されていた場合
-      if params[schedule.id.to_s]
-        contents = Content.where(schedule_id: schedule.id)
-        contents.each do |content|
-          entry_content = user_entry_content?(event_participation_id, content.id)
-          if entry_content.blank?
-            if content.id = params[schedule.id.to_s]
-              content_participation = ContentParticipation.new(event_participation_id: event_participation_id, content_id: content.id)
-              content_participation.save!
-            end
-          else
-            entry_content.each do |content|
-              content.destroy
+      # 参加コンテンツを登録
+      event_participation_id = user_entry?(params[:id], current_user.id).first.id
+      schedules = Schedule.where(event_id: params[:id])
+      schedules.each do |schedule|
+        # ラジオボタンで選択されていた場合
+        if params[schedule.id.to_s]
+          contents = Content.where(schedule_id: schedule.id)
+          contents.each do |content|
+            entry_content = user_entry_content?(event_participation_id, content.id)
+            if entry_content.blank?
+              if content.id = params[schedule.id.to_s]
+                content_participation = ContentParticipation.new(event_participation_id: event_participation_id, content_id: content.id)
+                content_participation.save!
+              end
+            else
+              entry_content.each do |content|
+                content.destroy
+              end
             end
           end
+        else
+          raise I18n.t("messages.error_select_content")
         end
-      else
-        raise "選択してません。"
       end
     end
+
     respond_to do |format|
-      format.html { redirect_to entry_event_path, notice: 'Time Table was successfully updated.' }
+      format.html { redirect_to entry_event_path, notice: I18n.t("messages.success_select_content") }
+      format.json { head :no_content }
+    end
+
+    rescue => e
+    respond_to do |format|
+      format.html { redirect_to entry_event_path, notice: e.message }
       format.json { head :no_content }
     end
   end
