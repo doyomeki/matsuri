e3fc28ebf2426c012a7e018345439f952b7e70d3
tenten0213 <takehito.0213@gmail.com>
2013-06-01 02:34:31 +0900

    room_idをattr_accessibleに追加

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,5 @@
 class Content < ActiveRecord::Base
-  attr_accessible :capacity, :description, :room_name, :schedule_id, :speaker_id, :title
+  attr_accessible :capacity, :description, :room_name, :schedule_id, :speaker_id, :title, :room_id
 
   belongs_to :schedule, foreign_key: "schedule_id"
 
