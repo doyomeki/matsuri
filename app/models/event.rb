221ee3da93ad3e62167c4899acbe292e678a0504
tenten0213 <takehito.0213@gmail.com>
2013-06-01 02:17:38 +0900

    eventのkeywordをattr_accessibleに追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :capacity, :description, :end_datetime, :place, :start_datetime, :title, :upcoming
+  attr_accessible :capacity, :description, :end_datetime, :place, :start_datetime, :title, :upcoming, :keyword
 
   has_many :schedules
 
