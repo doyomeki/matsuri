fe9e3d7bf07fe1c80defe5bd387e721222be30e1
tenten0213 <takehito.0213@gmail.com>
2013-06-03 03:20:37 +0900

    speakerから写真を取得し表示するように修正

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,4 +1,4 @@
 class Speaker < ActiveRecord::Base
-  attr_accessible :description, :name, :photo, :division, :retained_photo
+  attr_accessible :description, :name, :photo, :division, :retained_photo, :remove_photo
   image_accessor :photo
 end
