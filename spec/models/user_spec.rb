b493bdd5c29a8ca5081e87756502b2daa659603d
tenten0213 <takehito.0213@gmail.com>
2013-06-04 06:47:05 +0900

    userに対するテストを修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe User do
@@ -5,7 +6,6 @@ describe User do
   before(:each) do
     @attr = {
       :name => "Example User",
-      :email => "user@example.com",
       :password => "changeme",
       :password_confirmation => "changeme"
     }
@@ -15,38 +15,28 @@ describe User do
     User.create!(@attr)
   end
 
-  it "should require an email address" do
-    no_email_user = User.new(@attr.merge(:email => ""))
-    no_email_user.should_not be_valid
-  end
+  describe "name validations" do
 
-  it "should accept valid email addresses" do
-    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
-    addresses.each do |address|
-      valid_email_user = User.new(@attr.merge(:email => address))
-      valid_email_user.should be_valid
+    before(:each) do
+      @user = User.new(@attr)
     end
-  end
 
-  it "should reject invalid email addresses" do
-    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
-    addresses.each do |address|
-      invalid_email_user = User.new(@attr.merge(:email => address))
-      invalid_email_user.should_not be_valid
+    it "should be unique values" do
+      @user.save!
+      @another_user = User.new(@attr)
+      @another_user.save
+      expect("そのニックネームは既に使われています。").to eq @another_user.errors.messages[:name][0]
     end
-  end
 
-  it "should reject duplicate email addresses" do
-    User.create!(@attr)
-    user_with_duplicate_email = User.new(@attr)
-    user_with_duplicate_email.should_not be_valid
-  end
+    it "should not be empty" do
+      @user.name = ""
+      @user.save
+      expect("ニックネームを入力してください。").to eq @user.errors.messages[:name][0]
+    end
 
-  it "should reject email addresses identical up to case" do
-    upcased_email = @attr[:email].upcase
-    User.create!(@attr.merge(:email => upcased_email))
-    user_with_duplicate_email = User.new(@attr)
-    user_with_duplicate_email.should_not be_valid
+    it "should have a password confirmation attribute" do
+      @user.should respond_to(:password_confirmation)
+    end
   end
 
   describe "passwords" do
@@ -69,17 +59,6 @@ describe User do
     it "should require a password" do
       User.new(@attr.merge(:password => "", :password_confirmation => "")).
         should_not be_valid
-    end
-
-    it "should require a matching password confirmation" do
-      User.new(@attr.merge(:password_confirmation => "invalid")).
-        should_not be_valid
-    end
-
-    it "should reject short passwords" do
-      short = "a" * 5
-      hash = @attr.merge(:password => short, :password_confirmation => short)
-      User.new(hash).should_not be_valid
     end
 
   end
